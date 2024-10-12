01f7328556639e37de5a1e4f76f0f16f8d237721
yassineelmiri <yassireelhima@gmail.com>
2024-10-13 00:27:50 +0100

    Fix: Update seance routes

---
+++ BACKEND/routes/seance.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const SeanceController = require("../controllers/SeanceController");
+
+router.post("/", SeanceController.addSeance);
+router.get("/", SeanceController.getSeances);
+router.get("/:id", SeanceController.getSingleSeancesCtrl);
+router.put("/:id", SeanceController.updateSeance);
+router.delete("/:id", SeanceController.deleteSeance);
+
+module.exports = router;
