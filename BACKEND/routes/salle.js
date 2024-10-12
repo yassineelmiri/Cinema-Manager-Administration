430d7042cb084a7b2c7fcce414e81b2d0b190862
yassineelmiri <yassireelhima@gmail.com>
2024-10-13 00:27:48 +0100

    Fix: Update salle routes

---
+++ BACKEND/routes/salle.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const SalleController = require("../controllers/SalleController");
+
+router.post("/", SalleController.addSalle);
+router.get("/", SalleController.getSalles);
+router.put("/:id", SalleController.updateSalle);
+router.delete("/:id", SalleController.deleteSalle);
+router.get("/count", SalleController.getSalleCount);
+module.exports = router;
