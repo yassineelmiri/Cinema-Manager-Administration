cb10fbfda251d96e2cab330fd593b64e90f90745
yassineelmiri <yassireelhima@gmail.com>
2024-10-13 16:12:28 +0100

    Ajout de données fictives pour les posts pour faciliter le développement et les tests.

---
+++ FRONTEND/src/Pages/posts/dummyData.jsx
@@ -0,0 +1,26 @@
+import img1 from "../../assets/img/card/1.png";
+import img2 from "../../assets/img/card/2.png";
+import img3 from "../../assets/img/card/3.png";
+export const posts = [
+  {
+    id: 1,
+    imgSrc: img1,
+    rating: 8.3,
+    title: "The Good Lord Bird",
+    list: ["Free", "Action", "2019"],
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+    rating: 8.1,
+    title: "The Dictator",
+    list: ["Free", "Comedy", "2012"],
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+    rating: 7.9,
+    title: "Another Movie",
+    list: ["Free", "Drama", "2021"],
+  },
+];
