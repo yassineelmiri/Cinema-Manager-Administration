da93ec4d73482d79fe41ea8df96170e2b3d8ec25
yassineelmiri <yassireelhima@gmail.com>
2024-10-18 15:38:36 +0100

    Ajout de la page Forgot Password

--- FRONTEND/src/components/login/forgotpassword.jsx
+++ FRONTEND/src/components/login/forgotpassword.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../../assets/css/main.css';
-import { toast } from "react-toastify"; // Pour afficher des notifications
+import "../../assets/css/main.css";
+import { toast } from "react-toastify";
 
-import logo from '../../assets/img/series/logo.png'; // Assurez-vous que le chemin est correct
-import bgImage from '../../assets/img/bg.jpg'; // Assurez-vous que le chemin est correct
+import logo from "../../assets/img/series/logo.png";
+import bgImage from "../../assets/img/bg.jpg";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -12,23 +12,25 @@ const ForgotPassword = () => {
   const formSubmitHandler = (e) => {
     e.preventDefault();
 
-    if (email.trim() === '') {
+    if (email.trim() === "") {
       return toast.error("Email is required");
     }
 
     console.log({ email });
-    // Vous pouvez ici envoyer la requête pour réinitialiser le mot de passe
   };
 
   return (
-    <div className="sign section--full-bg" style={{ backgroundImage: `url(${bgImage})` }}>
+    <div
+      className="sign section--full-bg"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <div className="container">
         <div className="row">
           <div className="col-12">
             <div className="sign__content">
               <form onSubmit={formSubmitHandler} className="sign__form">
                 <Link to="/" className="sign__logo">
-                  <img src={logo} alt="Logo" /> {/* Affichage du logo */}
+                  <img src={logo} alt="Logo" /> 
                 </Link>
                 <div className="sign__group">
                   <input
@@ -36,15 +38,15 @@ const ForgotPassword = () => {
                     className="sign__input"
                     placeholder="Enter your email"
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)} // Mise à jour de l'état email
+                    onChange={(e) => setEmail(e.target.value)} 
                   />
                 </div>
-
-                <button className="sign__btn" type="submit">Reset Password</button> {/* Bouton de réinitialisation */}
-
+                <button className="sign__btn" type="submit">
+                  Reset Password
+                </button>{" "}
                 <span className="sign__delimiter">or</span>
                 <span className="sign__text">
-                  Remembered your password? <Link to="/signin">Sign in!</Link> {/* Lien vers la connexion */}
+                  Remembered your password? <Link to="/signin">Sign in!</Link>{" "}
                 </span>
               </form>
             </div>
