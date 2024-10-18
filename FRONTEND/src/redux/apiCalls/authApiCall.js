a44906f4028dfc644ce91a5123ca5dd4d84c45a5
yassineelmiri <yassireelhima@gmail.com>
2024-10-18 15:39:53 +0100

    Ajout des appels API pour l'authentification

--- FRONTEND/src/redux/apiCalls/authApiCall.js
+++ FRONTEND/src/redux/apiCalls/authApiCall.js
@@ -34,33 +34,40 @@ export function logoutUser() {
 
 //REGISTER USER
 export function registerUser(user) {
-    return async (dispatch) => {
-      try {
-        const { data } = await request.post("/api/auth/register", user);
-        dispatch(authActions.Register(data.message)); 
-        toast.success("Send Email successfully!");
-
-      } catch (error) {
-        if (error.response && error.response.data && error.response.data.message) {
-          toast.error(error.response.data.message);
-        } else {
-          toast.error("Something went wrong. Please try again."); 
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await request.post("/api/auth/register", user);
+      dispatch(authActions.Register(data.message));
+      toast.success("Send Email successfully!");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Something went wrong. Please try again.");
       }
-    };
+    }
+  };
 }
 
 //Verify Email
-export function verifyEmail(userId,token) {
+export function verifyEmail(userId, token) {
   return async (dispatch) => {
     try {
       await request.get(`/api/auth/${userId}/verify/${token}`);
-      dispatch(authActions.setIsEmailVerified()); 
+      dispatch(authActions.setIsEmailVerified());
     } catch (error) {
-      if (error.response && error.response.data && error.response.data.message) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
         toast.error(error.response.data.message);
       } else {
-        toast.error("Something went wrong. Please try again."); 
+        toast.error("Something went wrong. Please try again.");
       }
     }
   };
