b9e0082ae522c6ccf8676f25da906e8274628308
yassineelmiri <yassireelhima@gmail.com>
2024-10-18 15:38:58 +0100

    Ajout de la page d'erreur 404

--- FRONTEND/src/Pages/404/error.jsx
+++ FRONTEND/src/Pages/404/error.jsx
@@ -1,23 +1,29 @@
-import bgImage from '../../assets/img/series/bg.jpg';
+import bgImage from "../../assets/img/series/bg.jpg";
 const Error = () => {
-    return (
-      <div className="page-404 section--full-bg" style={{ backgroundImage: `url(${bgImage})` }}>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="page-404__wrap">
-                <div className="page-404__content">
-                  <h1 className="page-404__title">404</h1>
-                  <p className="page-404__text">The page you are looking for is not available!</p>
-                  <a href="/" className="page-404__btn">Go Back</a>
-                </div>
+  return (
+    <div
+      className="page-404 section--full-bg"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="page-404__wrap">
+              <div className="page-404__content">
+                <h1 className="page-404__title">404</h1>
+                <p className="page-404__text">
+                  The page you are looking for is not available!
+                </p>
+                <a href="/" className="page-404__btn">
+                  Go Back
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
-  export default Error;
-  
+    </div>
+  );
+};
+
+export default Error;
