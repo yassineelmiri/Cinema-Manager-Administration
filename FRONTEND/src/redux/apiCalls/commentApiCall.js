a3e08cd055b1f436b845b8a8e957059d7c513404
yassineelmiri <yassireelhima@gmail.com>
2024-10-22 16:47:08 +0100

    Refactorisation du code :\nMise à jour du fichier commentApiCall.js\nAmélioration des appels API pour les commentaires.

--- FRONTEND/src/redux/apiCalls/commentApiCall.js
+++ FRONTEND/src/redux/apiCalls/commentApiCall.js
@@ -1,7 +1,7 @@
 import { commentActions } from "../slices/commentSlice";
 import { toast } from "react-toastify";
 import request from "../../utils/request";
-import { setLoading } from "../slices/postSlice";
+
 
 // Créer un commentaire
 export function createComment(newComment) {
@@ -36,3 +36,63 @@ export function fetchComments() {
     }
   };
 }
+
+// Mettre à jour un commentaire
+export function updateComment(id, updatedComment) {
+  return async (dispatch) => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (!userInfo || !userInfo.token) {
+      toast.error("User is not authenticated");
+      return;
+    }
+
+    // Regroupez les données à envoyer dans le corps de la requête
+    const updateData = {
+      user: updatedComment.user._id,
+      text: updatedComment.text
+    };
+
+    try {
+      // Envoyez l'objet avec les données au lieu d'arguments séparés
+      const { data } = await request.put(`/api/comments/${id}`, updateData, {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
+
+      // Mettez à jour l'état avec les nouvelles données
+      dispatch(commentActions.updateComment(data));
+      toast.success("Commentaire modifié");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erreur lors de la modification du commentaire");
+    }
+  };
+}
+
+
+
+// Supprimer un commentaire
+export function deleteComment(id) {
+
+  return async (dispatch) => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (!userInfo || !userInfo.token) {
+      toast.error("User is not authenticated");
+      return;
+    }
+    try {
+      await request.delete(`/api/comments/${id}`,{
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
+      dispatch(commentActions.removeComment(id));
+      toast.success("Commentaire supprimé");
+    } catch (error) {
+      toast.error("Erreur lors de la suppression du commentaire");
+    }
+  };
+}
+
