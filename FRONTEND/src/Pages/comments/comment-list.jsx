ad0ab3d69826e5ba1d28e838b27fbcdf6095ed64
yassineelmiri <yassireelhima@gmail.com>
2024-10-22 16:46:42 +0100

    Refactorisation du code :\nMise à jour du fichier comment-list.jsx\nOptimisation de l'affichage des commentaires.

--- FRONTEND/src/Pages/comments/comment-list.jsx
+++ FRONTEND/src/Pages/comments/comment-list.jsx
@@ -1,37 +1,42 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchComments } from "../../redux/apiCalls/commentApiCall";
+import { fetchComments, updateComment, deleteComment } from "../../redux/apiCalls/commentApiCall";
 
-const CommentList = ({ postId }) => {
-  // Ajout du paramètre postId
+const CommentList = ({ postId , userId}) => {
   const dispatch = useDispatch();
   const comments = useSelector((state) => state.comment.comments);
+
 
   useEffect(() => {
     dispatch(fetchComments());
   }, [dispatch]);
 
+  const handleUpdate = (comment) => {
+    const updatedText = prompt("Modifier le commentaire :", comment.text);
+    if (updatedText) {
+      dispatch(updateComment(comment._id, { text: updatedText, user: comment.user }));
+    }
+  };
+
+  const handleDelete = (commentId) => {
+    if (window.confirm("Êtes-vous sûr de vouloir supprimer ce commentaire ?")) {
+      dispatch(deleteComment(commentId));
+    }
+  };
+
   if (!comments || comments.length === 0) {
     return <div>Aucun commentaire trouvé</div>;
   }
 
-  // Filtrer les commentaires par postId
-  const filteredComments = comments.filter(
-    (comment) => comment.postId === postId
-  );
+  const filteredComments = comments.filter((comment) => comment.postId === postId);
 
   return (
     <div className="comment-section">
-      <h1 className="article__trailer">Comments :</h1>
-
       {filteredComments.length === 0 ? (
         <p>Aucun commentaire pour ce post.</p>
       ) : (
         filteredComments.map((comment) => (
-          <div
-            key={comment._id}
-            className="comments__item comments__item--quote"
-          >
+          <div key={comment._id} className="comments__item comments__item--quote">
             <div className="comments__autor">
               <img src="" alt="" className="comments__avatar" />
               <span className="comments__name">{comment.username}</span>
@@ -40,12 +45,24 @@ const CommentList = ({ postId }) => {
               </span>
             </div>
             <p className="comments__text">{comment.text}</p>
-            <div className="comment-section__actions">
-              <i className="categories__item">Modifier</i>
-              <i className="categories__item">Supprimer</i>
-            </div>
+        
+            {/* Vérifier si l'utilisateur est le propriétaire du commentaire */}
+            {userId === comment.user._id && (
+              <div className="article__btns">
+                <button className="article__btn" onClick={() => handleUpdate(comment)}>
+                  Modifier
+                </button>
+                <button
+                  className="article__btn article__btn--white"
+                  onClick={() => handleDelete(comment._id)}
+                >
+                  Supprimer
+                </button>
+              </div>
+            )}
           </div>
         ))
+        
       )}
     </div>
   );
