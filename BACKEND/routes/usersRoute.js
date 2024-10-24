e2640e3690321ee545e538210753f0480b050198
yassineelmiri <yassireelhima@gmail.com>
2024-10-24 17:13:07 +0100

    Ajout du fichier usersRoute.js : Routes pour gérer les utilisateurs.

--- BACKEND/routes/usersRoute.js
+++ BACKEND/routes/usersRoute.js
@@ -11,22 +11,17 @@ const {
   verifyTokenAndOnlyUser,
   VerifyToken,
 } = require("../middlewares/verifyToken");
+
 const validateObjectId = require("../middlewares/validateObjectId");
 const photoUpload = require("../middlewares/photoUpload");
-
-
-
 router.route("/profile").get(verifyTokenAndAdmin, getAllUsersCtrl);
-
-router.route("/profile/:id")
+router
+  .route("/profile/:id")
   .get(validateObjectId, getUsersProfileCtrl)
   .put(validateObjectId, verifyTokenAndOnlyUser, updateUserProfileCtrl);
-
-
-
-router.route("/profile/profile-photo-upload").post(VerifyToken,photoUpload.single("image") ,profilePhotoUploadCtrl);
-
-
+router
+  .route("/profile/profile-photo-upload")
+  .post(VerifyToken, photoUpload.single("image"), profilePhotoUploadCtrl);
 router.route("/count").get(verifyTokenAndAdmin, getUsersCountCtrl);
 
 module.exports = router;
