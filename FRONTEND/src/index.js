308eafc9ceefafa02ce0f5cd021a9dcf2a768235
yassineelmiri <yassireelhima@gmail.com>
2024-10-24 17:13:17 +0100

    Ajout du fichier index.js : Point d'entrée de l'application React.

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -6,7 +6,9 @@ import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
     <App />
