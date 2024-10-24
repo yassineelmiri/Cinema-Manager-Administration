da331235459ef9410d7a30085471e2eef4038f57
yassineelmiri <yassireelhima@gmail.com>
2024-10-24 17:13:06 +0100

    Ajout du fichier seance.js : Routes pour gérer les séances de cinéma.

--- BACKEND/routes/seance.js
+++ BACKEND/routes/seance.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const SeanceController = require("../controllers/SeanceController");
 
+
 router.post("/", SeanceController.addSeance);
 router.get("/", SeanceController.getSeances);
 router.get("/:id", SeanceController.getSingleSeancesCtrl);
