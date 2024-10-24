6c803f88149f9d847966d2621d057c95521ebc49
yassineelmiri <yassireelhima@gmail.com>
2024-10-24 17:12:58 +0100

    Ajout du fichier photoUpload.js : Middleware pour gérer le téléchargement des photos et leur stockage.

--- BACKEND/middlewares/photoUpload.js
+++ BACKEND/middlewares/photoUpload.js
@@ -25,7 +25,7 @@ const photoUpload = multer({
       cb({ message: "Unsupported file format" }, false);
     }
   },
-  limits: { fileSize: 1024 * 1024 }, //1 megabyte
+  limits: { fileSize: 1024 * 1024 },
 });
 
 module.exports = photoUpload;
