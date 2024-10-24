0344a8cef7996418053ee7b56d1fa26d9d0d659c
yassine elmiri <144792335+yassineelmiri@users.noreply.github.com>
2024-10-24 17:16:57 +0100

    Delete BACKEND directory

--- BACKEND/routes/commentsRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { createCommentCtrl, getCommentsCtrl } = require("../controllers/commentsController");
-
-router.post("/", createCommentCtrl);
-router.get("/", getCommentsCtrl);
-
-module.exports = router;
--- BACKEND/routes/reservation.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ReservationController = require("../controllers/ReservationController");
-
-router.post("/",  ReservationController.makeReservation);
-router.delete("/:id", ReservationController.cancelReservation);
-router.get("/",  ReservationController.getReservations);
-
-module.exports = router;
