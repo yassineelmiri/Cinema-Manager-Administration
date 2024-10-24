82eced723446881ad6e6951e19cbfeab86cb7b86
yassineelmiri <yassireelhima@gmail.com>
2024-10-24 17:12:59 +0100

    Ajout du fichier validateObjectId.js : Middleware pour valider les identifiants d'objets MongoDB dans les requêtes.

--- BACKEND/middlewares/validateObjectId.js
+++ BACKEND/middlewares/validateObjectId.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 module.exports = (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.status(400).json({ message: "invalid id" });
